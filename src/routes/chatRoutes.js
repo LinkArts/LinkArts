c858af83f362522324c2385c6984ea06eee9b961
Gustavo <gustavoog05@hotmail.com>
2024-09-20 19:16:52 -0300

    Delete src directory

--- src/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ChatController = require('../controllers/chatController')
-
-router.get('/conversar/:id', ChatController.abrirConversas)
-
-module.exports = router
