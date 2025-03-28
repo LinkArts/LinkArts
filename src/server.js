5b93b8a0242747430417065f1fdae4d808a9a976
Kevin <kevincaireleandro@hotmail.com>
2025-03-28 18:08:53 -0300

    Reset dos Arquivos

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+const app = express();
+
+//CONFIGURANDO
